6d63f38a7f46d824af9bd1728248227e23438b70
Md. Minhajul Islam <47735234+minhajk1001@users.noreply.github.com>
2023-11-22 02:00:53 +0600

    back button solved home

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,88 @@
+// import 'package:aminahub/screens/home/components/body.dart';
+// import 'package:flutter/material.dart';
+// import 'package:geocoding/geocoding.dart';
+// import 'package:geolocator/geolocator.dart';
+// import '../../components/custom_btm_nav_bar.dart';
+// import '../../enums.dart';
+// import '../selectLocation/select_city.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   static const String routeName = "/home";
+
+//   @override
+//   _HomeScreenState createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   String currentCity = "Loading...";
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _getLocation();
+//   }
+
+//   void _getLocation() async {
+//     LocationPermission permission = await Geolocator.checkPermission();
+//     if (permission == LocationPermission.denied) {
+//       permission = await Geolocator.requestPermission();
+//     }
+
+//     if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+//       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+//       List<Placemark> placeMarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+
+//       if (placeMarks.isNotEmpty) {
+//         Placemark placeMark = placeMarks[0];
+//         String? city = placeMark.locality;
+//         setState(() {
+//           currentCity = city ?? "failed";
+//         });
+//       }
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         automaticallyImplyLeading: false,
+//         centerTitle: true,
+//         title: Text(
+//           currentCity,
+//           style: const TextStyle(
+//             fontSize: 18,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//       ),
+//       body: HomeBody(),
+//       bottomNavigationBar: const CustomBottomNavBar(selectedMenu: MenuState.home),
+//       floatingActionButton: FloatingActionButton(
+//         backgroundColor: const Color(0xFF80D0D9),
+//         onPressed: () {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(
+//               builder: (context) => SelectLocation(currentCity),
+//             ),
+//           ).then((selectedCity) {
+//             if (selectedCity != null) {
+//               setState(() {
+//                 currentCity = selectedCity;
+//               });
+//             }
+//           });
+//         },
+//         child: const Icon(
+//           Icons.location_on,
+//           color: Color(0XFFFAAB37),
+//           ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:aminahub/screens/home/components/body.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
@@ -8,6 +93,8 @@ import '../selectLocation/select_city.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/home";
+
+  const HomeScreen({super.key});
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -44,42 +131,47 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Text(
-          currentCity,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+    return WillPopScope(
+      onWillPop: () async {
+        // Disable the back button.
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Text(
+            currentCity,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      body: HomeBody(),
-      bottomNavigationBar: const CustomBottomNavBar(selectedMenu: MenuState.home),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xFF80D0D9),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => SelectLocation(currentCity),
-            ),
-          ).then((selectedCity) {
-            if (selectedCity != null) {
-              setState(() {
-                currentCity = selectedCity;
-              });
-            }
-          });
-        },
-        child: const Icon(
-          Icons.location_on,
-          color: Color(0XFFFAAB37),
+        body: HomeBody(),
+        bottomNavigationBar: const CustomBottomNavBar(selectedMenu: MenuState.home),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: const Color(0xFF80D0D9),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SelectLocation(currentCity),
+              ),
+            ).then((selectedCity) {
+              if (selectedCity != null) {
+                setState(() {
+                  currentCity = selectedCity;
+                });
+              }
+            });
+          },
+          child: const Icon(
+            Icons.location_on,
+            color: Color(0XFFFAAB37),
           ),
+        ),
       ),
     );
   }
 }
-
